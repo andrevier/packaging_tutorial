47eabf1a18faec2bdbb61b1665f2af78be7b0911
andrevier <rangel_andre@outlook.com>
2022-05-14 10:23:24 -0300

    (1) commit in the test1 branch.

--- example_pkg/mod.py
+++ example_pkg/mod.py
@@ -1,6 +1,8 @@
 # Branch master
 
-print('Hi, I am an student of python. I have a lot to do.')
+print('Hi, I am an student of python. I have a lot to do in another branch too.')
 a = 0
+b = 1
 for i in range(5):
     a += 1
+    b = a + 1
